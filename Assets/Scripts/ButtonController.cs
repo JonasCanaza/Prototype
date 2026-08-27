070b8db7ed6b71c28454238e1d9181fe07d80fb2
unknown <Jonas>
2026-08-27 13:04:08 -0300

    Fix button collider

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
@@ -6,29 +8,62 @@ public class ButtonController : MonoBehaviour
     [SerializeField] private LayerMask activators;
     [SerializeField] private Door door;
 
-    private bool isPressed;
+    //Collider2D activationCollider;
+    List<Collider2D> activatorColliderList = new List<Collider2D>();
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collider)
     {
-        if (((1 << collision.gameObject.layer) & activators) != 0)
+        //if (activationCollider != null)
+        //{
+        //    return;
+        //}
+
+        //if (((1 << collider.gameObject.layer) & activators) != 0)
+        //{
+        //    activatorColliders.Add(collider);
+        //    //activationCollider = collider;
+
+        //    door.SetOpen(true);
+
+        //    Debug.Log("Boton presionado!");
+        //}
+
+        if (((1 << collider.gameObject.layer) & activators) != 0)
         {
-            isPressed = true;
+            activatorColliderList.Add(collider);
+        }
 
+        if (activatorColliderList.Count >= 1)
+        {
             door.SetOpen(true);
-
-            Debug.Log("Boton presionado!");
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerExit2D(Collider2D collider)
     {
-        if (((1 << collision.gameObject.layer) & activators) != 0)
+        //if (activationCollider != collider)
+        //{
+        //    return;
+        //}
+
+        //if (((1 << collider.gameObject.layer) & activators) != 0)
+        //{
+        //    activatorColliders.Remove(collider);
+        //    //activationCollider = null;
+
+        //    door.SetOpen(false);
+
+        //    Debug.Log("Boton sin presionar!");
+        //}
+
+        if (((1 << collider.gameObject.layer) & activators) != 0)
         {
-            isPressed = false;
+            activatorColliderList.Remove(collider);
+        }
 
+        if (activatorColliderList.Count <= 0)
+        {
             door.SetOpen(false);
-
-            Debug.Log("Boton sin presionar!");
         }
     }
 }
